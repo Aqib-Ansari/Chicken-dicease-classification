c2377044caf965739eaad9db57fb1451010488ce
Aqib-Ansari <aqibansari22298@gmail.com>
2023-10-22 12:18:40 +0530

    deleted artifacts to check modular code for data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from cnn_classifier_chicken import logger
 
-logger.info("Welcome to my custom logging ")
+from cnn_classifier_chicken.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipline
+
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f"\n >>>> stage {STAGE_NAME} started >>>>>>>> \n")
+    obj = DataIngestionTrainingPipline()
+    obj.main()
+    logger.info(f"\n >>>>>> stage {STAGE_NAME} Completed >>>>>>>>>>> \n\n============================x")
+except Exception as e:
+    raise e
+            
